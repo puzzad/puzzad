210e3a493cb6bcc93c6766fa3f7f0926647324a1
csmith <csmith@users.noreply.github.com>
2022-11-05 11:49:34 +0000

    Deploying to gh-pages from @ Puzzad/puzzad@a687536b1dd4c7552c6c426253acdc11a7ec1965 🚀

--- assets/Home.5a0968ab.js
+++ assets/Home.5a0968ab.js
@@ -1,18 +1,18 @@
-import{S as A,i as q,s as L,h as n,j as v,r as t,a as z,w as a,a3 as C,C as H,a4 as P,n as I,d as b,_ as T,B as $}from"./index.50bbf899.js";function j(p){let l,h,e,i,y,r,f,w,u,o,x,c,_,d,g,k;return{c(){l=n("h2"),l.textContent="Puzzad: Puzzle Adventures",h=v(),e=n("main"),i=n("section"),i.innerHTML=`<h3 class="svelte-zxgmb4">Ready to start an adventure?</h3> 
-        <a href="/#/Adventures" class="svelte-zxgmb4">Let&#39;s go \xBB</a>`,y=v(),r=n("section"),f=n("h3"),f.textContent="Got a game code?",w=v(),u=n("form"),o=n("input"),x=v(),c=n("input"),_=v(),d=n("section"),d.innerHTML=`<h3 class="svelte-zxgmb4">What is a puzzle adventure?</h3> 
+import{S as I,i as x,s as A,h as n,j as v,r as t,a as z,w as a,a3 as k,C,a4 as L,n as H,d as w,_ as P,B as T}from"./index.2ee0aec2.js";function $(c){let l,p,e,i,m,r,y,q,u,o,j,h,_,d,g,b;return{c(){l=n("h2"),l.textContent="Puzzad: Puzzle Adventures",p=v(),e=n("main"),i=n("section"),i.innerHTML=`<h3 class="svelte-1fqfvjc">Ready to start an adventure?</h3> 
+        <a href="/#/Adventures" class="svelte-1fqfvjc">Let&#39;s go \xBB</a>`,m=v(),r=n("section"),y=n("h3"),y.textContent="Got a game code?",q=v(),u=n("form"),o=n("input"),j=v(),h=n("input"),_=v(),d=n("section"),d.innerHTML=`<h3 class="svelte-1fqfvjc">What is a puzzle adventure?</h3> 
         <p>A puzzle adventure is part escape room, part puzzle hunt.
             Each adventure is a story interleaved with a variety of puzzles.
             To progress from one part of the story to the next you have to
             solve the puzzle you&#39;re presented with \u2014 but don&#39;t worry,
             there are ample hints available if you get stuck!</p> 
-        <h3 class="svelte-zxgmb4">Is there a time limit?</h3> 
+        <h3 class="svelte-1fqfvjc">Is there a time limit?</h3> 
         <p>Nope! Puzzle adventures are completely self-driven. If you want
             to stop and carry on later that&#39;s not a problem. Want to treat
             it like an advent calendar and open one door a day? Go ahead!</p> 
         <p>For those who are more competitive we&#39;ll show your total solve
             time when you finish the adventure (along with number of hints
             used) so you can compare against your friends.</p> 
-        <h3 class="svelte-zxgmb4">Can I play with friends?</h3> 
+        <h3 class="svelte-1fqfvjc">Can I play with friends?</h3> 
         <p>Yes, and we actively encourage it! When you start an adventure
             you&#39;ll get a code that looks something like <code>revolving-magenta-ocelot</code>.
             Simply give that code to your friends, they put it in the box
@@ -22,7 +22,7 @@ import{S as A,i as q,s as L,h as n,j as v,r as t,a as z,w as a,a3 as C,C as H,a4
             and so on). You can see your teammate&#39;s guesses in real time,
             but it helps if you can ask them <em>why</em> they keep guessing
             &quot;elephant&quot;.</p> 
-        <h3 class="svelte-zxgmb4">How difficult is it?</h3> 
+        <h3 class="svelte-1fqfvjc">How difficult is it?</h3> 
         <p>Our starter adventure, Conspiracy, is designed to be around the
             difficulty of a medium escape room. Having some knowledge of
             typical escape rooms or puzzle hunts challenges will help, but
@@ -34,4 +34,4 @@ import{S as A,i as q,s as L,h as n,j as v,r as t,a as z,w as a,a3 as C,C as H,a4
             hunts or thrives on the more difficult escape rooms, we recommend
             avoiding all hints and seeing how quickly you can complete the
             full adventure. Alternatively, you could use it as a gentle introduction
-            to rope some less puzzle-inclined friends into the hobby!</p>`,t(i,"id","hero"),t(i,"class","svelte-zxgmb4"),t(f,"class","svelte-zxgmb4"),t(o,"type","text"),t(o,"placeholder","revolving-magenta-ocelot"),t(o,"class","svelte-zxgmb4"),t(c,"type","submit"),c.value="Play!",t(c,"class","svelte-zxgmb4"),t(u,"class","svelte-zxgmb4"),t(r,"id","play"),t(r,"class","svelte-zxgmb4"),t(d,"id","intro"),t(d,"class","svelte-zxgmb4"),t(e,"class","svelte-zxgmb4")},m(s,m){z(s,l,m),z(s,h,m),z(s,e,m),a(e,i),a(e,y),a(e,r),a(r,f),a(r,w),a(r,u),a(u,o),C(o,p[0]),a(u,x),a(u,c),a(e,_),a(e,d),g||(k=[H(o,"input",p[2]),H(u,"submit",P(p[1]))],g=!0)},p(s,[m]){m&1&&o.value!==s[0]&&C(o,s[0])},i:I,o:I,d(s){s&&b(l),s&&b(h),s&&b(e),g=!1,T(k)}}}function G(p,l,h){let e="";const i=()=>{$("/game/"+e.toLowerCase().replaceAll(/[^a-z-]/g,""))};function y(){e=this.value,h(0,e)}return[e,i,y]}class W extends A{constructor(l){super(),q(this,l,G,j,L,{})}}export{W as default};
+            to rope some less puzzle-inclined friends into the hobby!</p>`,t(i,"id","hero"),t(i,"class","svelte-1fqfvjc"),t(y,"class","svelte-1fqfvjc"),t(o,"type","text"),t(o,"placeholder","revolving-magenta-ocelot"),t(o,"class","svelte-1fqfvjc"),t(h,"type","submit"),h.value="Play!",t(h,"class","svelte-1fqfvjc"),t(u,"class","svelte-1fqfvjc"),t(r,"id","play"),t(r,"class","svelte-1fqfvjc"),t(d,"id","intro"),t(d,"class","svelte-1fqfvjc"),t(e,"class","svelte-1fqfvjc")},m(s,f){z(s,l,f),z(s,p,f),z(s,e,f),a(e,i),a(e,m),a(e,r),a(r,y),a(r,q),a(r,u),a(u,o),k(o,c[0]),a(u,j),a(u,h),a(e,_),a(e,d),g||(b=[C(o,"input",c[2]),C(u,"submit",L(c[1]))],g=!0)},p(s,[f]){f&1&&o.value!==s[0]&&k(o,s[0])},i:H,o:H,d(s){s&&w(l),s&&w(p),s&&w(e),g=!1,P(b)}}}function G(c,l,p){let e="";const i=()=>{T("/game/"+e.toLowerCase().replaceAll(/[^a-z-]/g,""))};function m(){e=this.value,p(0,e)}return[e,i,m]}class W extends I{constructor(l){super(),x(this,l,G,$,A,{})}}export{W as default};
