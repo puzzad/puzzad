7712e65f36f259b523ce56aee53b7c190d4f6eaa
Chris Smith <chris@chameth.com>
2022-11-06 22:01:20 +0000

    Move puzzle content styles into PuzzleContent

--- src/components/PuzzleContent.svelte
+++ src/components/PuzzleContent.svelte
@@ -37,6 +37,50 @@
         .then(([content, ...urls]) => content.replace(embedRegex, () => urls.shift()))
 </script>
 
+<style global>
+    section.tip {
+        position: relative;
+        border-top: 10px solid #e3bc5e;
+        border-bottom: 2px solid #e3bc5e;
+        background-color: rgba(227, 188, 94, .3);
+        padding: 0 0.7em;
+    }
+
+    section.tip::before {
+        float: right;
+        border-bottom-right-radius: 5px;
+        border-bottom-left-radius: 5px;
+        padding: 5px 10px;
+        margin: 0 20px;
+        content: "ℹ️ information";
+        font-weight: bold;
+        font-variant: small-caps;
+        background-color: #e3bc5e;
+        color: black;
+    }
+
+    section.story {
+        position: relative;
+        border-top: 10px solid #1c76c5;
+        border-bottom: 2px solid #1c76c5;
+        background-color: rgba(28, 118, 197, .3);
+        padding: 0 0.7em;
+    }
+
+    section.story::before {
+        float: right;
+        border-bottom-right-radius: 5px;
+        border-bottom-left-radius: 5px;
+        padding: 5px 10px;
+        margin: 0 20px;
+        content: "📖 story";
+        font-weight: bold;
+        font-variant: small-caps;
+        background-color: #1c76c5;
+        color: white;
+    }
+</style>
+
 {#await html}
     <Spinner/>
 {:then html}
