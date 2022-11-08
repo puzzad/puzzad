630fb2b3179a35710c7179ca5b3326e3f3ba2a28
Chris Smith <chris@chameth.com>
2022-11-08 15:21:45 +0000

    Make the game page use a promise

--- src/routes/games/[code]/index.svelte
+++ src/routes/games/[code]/index.svelte
@@ -1,91 +1,55 @@
 <script>
   import AdventureLogo from '$comps/AdventureLogo.svelte'
-  import {getGameClient} from '$lib/db.ts'
+  import {getGameClient} from '$lib/db'
+  import {formatDuration} from '$lib/time'
   import Spinner from '$comps/Spinner.svelte'
   import {title} from '$lib/title.ts'
   import {goto} from '@roxi/routify'
-  import {onMount} from 'svelte'
   import {params} from '@roxi/routify'
+  import Error from '$comps/Error.svelte'
 
-  $: code = $params.code
-  let data = {}
-  let initial = true
   let stats
-  let displayError
-  let gc
 
-  onMount(async function() {
-    try {
-      gc = await getGameClient(code)
-    } catch (e) {
-      displayError = e.message || e
-      initial = false
-      return
-    }
-
-    let {data: game, error} =
-        await gc.from('games').
-            select('status, puzzle, startTime, endTime, adventures ( name, description)').
-            eq('code', code)
-
-    if (game[0]?.adventures) {
-      data = game[0]
-      stats = gc.rpc('getstats', {gamecode: code}).then(({data: stats, error}) => {
-        if (error) {
-          return []
-        } else {
-          return stats
+  let game = getGameClient($params.code).
+      then((gc) =>
+          gc.from('games').
+              select('status, puzzle, startTime, endTime, adventures ( name, description)').
+              eq('code', $params.code).
+              throwOnError().
+              single(),
+      ).
+      then(({data: game}) => game).
+      then((game) => {
+        title.set(`Puzzad: ${game.adventures.name} - ${$params.code}`)
+        if (game.status === 'EXPIRED') {
+          stats = fetchStats(game.startTime)
         }
-      }).then((rows) => {
-        let lastTime = data.startTime
+        return game
+      })
+
+  const fetchStats = async (startTime) => getGameClient($params.code).
+      then((gc) => gc.rpc('getstats', {gamecode: $params.code}).throwOnError()).
+      then(({data: stats}) => stats).
+      then((rows) => {
+        let lastTime = startTime
         rows.forEach((r) => {
-          r.time = delta(lastTime, r.solvetime)
+          r.time = formatDuration(lastTime, r.solvetime)
           lastTime = r.solvetime
         })
         return rows
       })
-      title.set('Puzzad: ' + game[0]?.adventures.name + ' - ' + code)
-    } else {
-      title.set('Puzzad: Game not found')
-      error = 'Unable to find game'
-    }
-    if (error) {
-      displayError = error
-    }
-    initial = false
-  })
 
-  const handleStartAdventure = async function() {
-    let {data: puzzle} = await gc.rpc('startadventure')
-    console.log(puzzle)
-    if (puzzle) {
-      $goto('/games/[code]/[puzzle]', {code: code, puzzle: puzzle})
-    }
-  }
+  const handleStartAdventure = async () =>
+      getGameClient($params.code).
+          then((gc) => gc.rpc('startadventure').throwOnError()).
+          then(({data: puzzle}) => {
+            if (puzzle) {
+              return $goto('/games/[code]/[puzzle]', {code: $params.code, puzzle: puzzle})
+            }
+          })
 
-  const handleContinueAdventure = async function() {
-    $goto('/games/[code]/[puzzle]', {code: code, puzzle: data.puzzle})
-  }
-
-  const delta = function(start, end) {
-    const diff = ((new Date(end)).getTime() - (new Date(start)).getTime()) / 1000
-    const text = (value, label) => {
-      const rounded = Math.floor(value)
-      if (rounded === 1) {
-        return `${rounded} ${label}`
-      } else if (rounded > 1) {
-        return `${rounded} ${label}s`
-      } else {
-        return ''
-      }
-    }
-
-    return [
-      text(diff % 60, 'second'),
-      text((diff / 60) % 60, 'minute'),
-      text((diff / 3600) % 24, 'hour'),
-      text(diff / 86400, 'day'),
-    ].filter((t) => t !== '').reverse().join(', ')
+  const handleContinueAdventure = async function(puzzle) {
+    $goto('/games/[code]/[puzzle]', {code: $params.code, puzzle})
   }
 </script>
 
@@ -113,18 +77,15 @@
   }
 </style>
 
-{#if initial}
+{#await game}
   <Spinner/>
-{:else if displayError || !data}
-  <h1>Error finding game</h1>
-  <p>{displayError}</p>
-{:else}
+{:then data}
   <h1>
-    <AdventureLogo bind:name={data.adventures.name}></AdventureLogo>
+    <AdventureLogo name={data.adventures.name}></AdventureLogo>
   </h1>
   {#if data.status === 'EXPIRED'}
     <p>Congratulations! You finished the adventure!</p>
-    <p>You took {delta(data.startTime, data.endTime)}!</p>
+    <p>You took {formatDuration(data.startTime, data.endTime)}!</p>
     {#await stats then puzzles}
       <table class="stats">
         <thead>
@@ -151,7 +112,7 @@
       Take this if you want to recruit others to help you:
     </p>
     <code>
-      {code}
+      {$params.code}
     </code>
     <p>
       Once you're ready, press below to go to the first puzzle in the adventure!
@@ -161,8 +122,19 @@
     </button>
   {:else if data.status === 'ACTIVE'}
     <p>This adventure is already in progress!</p>
-    <button on:click={() => handleContinueAdventure()}>
+    <button on:click={() => handleContinueAdventure(data.puzzle)}>
       Go to the current puzzle
     </button>
   {/if}
-{/if}
+{:catch error}
+  {#if error.message === 'Invalid team code'}
+    <h1>Game not found</h1>
+    <p>
+      The adventure you are seeking does not seem to exist.
+      If you have been given a code, please double check your spelling.
+      If you registered the adventure yourself, you can find it listed in <a href="/games">My Games</a>.
+    </p>
+  {:else}
+    <Error error={error}></Error>
+  {/if}
+{/await}
