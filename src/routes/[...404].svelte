3ecc89191b71416155c5cf67de44c80cd2b383de
Greg Holmes <git@greg.holmes.name>
2022-11-08 18:16:39 +0000

    Add 404 page

---
+++ src/routes/[...404].svelte
@@ -0,0 +1,14 @@
+<script>
+    import {title} from '$lib/title.ts'
+    title.set("Puzzad: Not found")
+    export let params = {}
+</script>
+<style>
+    section {
+        display: flex;
+        justify-content: center;
+    }
+</style>
+<section>
+    Not found
+</section>
