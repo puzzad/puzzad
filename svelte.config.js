485bdff3881dac3862904c09f0d43bbb5aa17044
Greg Holmes <git@greg.holmes.name>
2022-11-09 23:57:35 +0000

    Not sure what these are, optimising sounds good though.

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,20 @@
-import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static'
+import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter({
-			pages: 'dist',
-			assets: 'dist',
-			fallback: 'index.html'
-		}),
-		prerender: { entries: [] }
-	}
-};
+  kit: {
+    adapter: adapter({
+      pages: 'dist',
+      assets: 'dist',
+      fallback: 'index.html',
+    }),
+    prerender: {
+      entries: [],
+    },
+  },
+}
 
-export default config;
+export default config
