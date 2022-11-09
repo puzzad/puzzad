6a2ed9ec79e5234ee0127844a207866b24393180
Greg Holmes <git@greg.holmes.name>
2022-11-09 22:00:56 +0000

    Fix titles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,9 +4,13 @@
     import bahunya from '$lib/assets/bahunya.min.css'
     import puzzad from '$lib/assets/puzzad.css'
     import {FlatToast, ToastContainer} from "svelte-toasts";
+    import {title} from '$lib/title.ts'
 
     export const ssr = false;
 </script>
+<svelte:head>
+  <title>{$title}</title>
+</svelte:head>
 <NavBar logo="{logo}"/>
 <slot/>
 <ToastContainer placement="bottom-right" theme="dark" let:data={data}>
