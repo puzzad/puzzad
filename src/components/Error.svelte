63c45ea084fc305e322b6ddb6ed76c12f093c944
Greg Holmes <git@greg.holmes.name>
2022-11-20 22:56:32 +0000

    Re-add sentry configuration (it really should be elsewhere)

--- src/components/Error.svelte
+++ src/components/Error.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
   import * as Sentry from '@sentry/svelte'
+  import {BrowserTracing} from '@sentry/tracing'
 
   export let error
 
   const reload = () => document.location.reload()
+
+  Sentry.init({
+    dsn: import.meta.env.PROD ?
+        'https://8d95b82da5804fdcaf8cbbeb7132edeb@glitch.puzzad.com/2' :
+        'https://6c58e17321094c88bd40d834fd1f687e@glitch.puzzad.com/1',
+    integrations: [new BrowserTracing()],
+    tracesSampleRate: import.meta.env.PROD ? 0.01 : 1.0,
+    environment: import.meta.env.PROD ? 'production' : 'development',
+  })
 
   $: if (error) {
     console.log(error)
