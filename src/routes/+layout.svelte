73740d9b2e2c9bf9e62f52f9e6dda7c8046cdd67
Greg Holmes <git@greg.holmes.name>
2022-11-20 22:59:42 +0000

    Move sentry init

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,17 @@
   import puzzad from '$assets/puzzad.css'
   import {FlatToast, ToastContainer} from 'svelte-toasts'
   import {title} from '$lib/title'
+  import * as Sentry from '@sentry/svelte'
+  import {BrowserTracing} from '@sentry/tracing'
   export const ssr = false;
+  Sentry.init({
+    dsn: import.meta.env.PROD ?
+        'https://8d95b82da5804fdcaf8cbbeb7132edeb@glitch.puzzad.com/2' :
+        'https://6c58e17321094c88bd40d834fd1f687e@glitch.puzzad.com/1',
+    integrations: [new BrowserTracing()],
+    tracesSampleRate: import.meta.env.PROD ? 0.01 : 1.0,
+    environment: import.meta.env.PROD ? 'production' : 'development',
+  })
 </script>
 <svelte:head>
   <title>{$title}</title>
