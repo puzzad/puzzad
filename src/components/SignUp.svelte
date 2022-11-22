fe53ed40640c7351dbae09e81211f90b4dd54b92
Chris Smith <chris@chameth.com>
2022-11-22 11:32:59 +0000

    Show password reqs on signup page

--- src/components/SignUp.svelte
+++ src/components/SignUp.svelte
@@ -27,12 +27,40 @@
 
 <style>
   .disclaimer {
+    grid-column: 1 / 3;
+    margin: 2em 0;
     font-size: small;
     color: #999999;
+  }
+
+  @media (min-width: 480px) {
+    div {
+      display: grid;
+      grid-template-columns: auto 40%;
+      grid-gap: 2em;
+    }
+
+    .disclaimer {
+      text-align: center;
+      border-top: 1px dotted #999999;
+      padding-top: 2em;
+    }
+  }
+
+  form {
+    align-content: center;
+  }
+
+  section {
+    background-color: #222222;
+    padding: 0.5em 1em;
+    margin: 0;
+    border-radius: 15px;
   }
 </style>
 
 <h2>Sign up</h2>
+<div>
 <form class="basic" on:submit|preventDefault={authAction}>
   <label for="email">E-mail:</label>
   <input
@@ -53,10 +81,28 @@
   <button type="submit">
     Sign up
   </button>
+  <p class="disclaimer">
+    If you create an account, we will only use your e-mail address to send you information
+    related to your account (e.g. password reset links and receipts). We won't use it for marketing
+    purposes unless you explicitly sign up for our mailing list, and we'll never sell your data or
+    give it to anyone who will abuse it.
+  </p>
 </form>
-<p class="disclaimer">
-  If you create an account, we will only use your e-mail address to send you information
-  related to your account (e.g. password reset links and receipts). We won't use it for marketing
-  purposes unless you explicitly sign up for our mailing list, and we'll never sell your data or
-  give it to anyone who will abuse it.
-</p>
+  <section>
+    <p>
+      To protect themselves from evil doers, adventurers must have
+      a password that:
+    </p>
+    <ul>
+      <li>is at least 8 characters long</li>
+      <li>contains an uppercase letter</li>
+      <li>contains a lowercase letter</li>
+      <li>contains a number</li>
+      <li>contains a special character</li>
+    </ul>
+    <p>
+      We highly recommend a capable assistant like <a href="https://bitwarden.com/">BitWarden</a>
+      for generating and securely storing passwords.
+    </p>
+  </section>
+</div>
