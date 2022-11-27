c5df5e154f0aa2315ab03f6a0920e9fb0e566777
Greg Holmes <git@greg.holmes.name>
2022-11-27 15:12:30 +0000

    Add captcha to contact form

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,33 +1,110 @@
 <script lang="ts">
-  import type { ActionData } from './$types';
-  import { enhance } from '$app/forms';
-  export let form: ActionData;
+  import {supabase} from '$lib/db.ts'
+  import SvelteHcaptcha from 'svelte-hcaptcha'
+  import Spinner from '$components/Spinner.svelte'
+  import {toasts} from 'svelte-toasts'
+
+  let name, nameError, email, emailError, message, messageError, success, captchaToken = '', supabaseToken, loggedIn
+  let loading = false
+  const handleSubmit = () => {
+    fetch(import.meta.env.VITE_SUPABASE_URL + '/mail/contact', {
+      method: 'POST',
+      headers: {
+        'Authorization': 'Bearer ' + supabaseToken,
+        'Content-Type': 'Application/json',
+      },
+      body: JSON.stringify({
+        'token': captchaToken,
+        'name': name,
+        'email': email,
+        'message': message,
+      }),
+    }).then(response => {
+      if (!response.ok) {
+        return response.json().then((j) => {
+          throw j.error
+        })
+      }
+      return response
+    }).then(_ => {
+      success = true
+      loading = false
+    }).catch(error => {
+      toasts.add({
+        title: 'Error submitting form',
+        description: error,
+        duration: 10000,
+        type: 'error',
+      })
+    })
+  }
+  supabase.auth.getSession().then(response => {
+    if (response.data.session && response.data.session.user.email) {
+      loggedIn = true
+      email = response.data.session.user.email
+      supabaseToken = response.data.session.access_token
+    } else {
+      loggedIn = false
+    }
+    loading = false
+  })
+  const captchaSuccess = (token) => {
+    captchaToken = token.detail.token
+  }
+  const captchaFail = () => {
+    captchaToken = ''
+  }
 </script>
 <style>
   form {
     display: grid;
   }
+
   input {
     margin-bottom: 1em;
   }
+
   button {
     margin-top: 1em;
     margin-right: 0;
   }
+
+  section :global(iframe) {
+    margin-top: 1em;
+  }
 </style>
-<h2>Contact us</h2>
-<form method="POST" use:enhance>
-  <label for="name">Name</label>
-  {#if form?.nameMissing}<p class="error">The name field is required</p>{/if}
-  <input disabled id="name" name="name" value="{form?.name ?? ''}" />
-  <label for="email">Email</label>
-  {#if form?.emailMissing}<p class="error">The email field is required</p>{/if}
-  <input disabled type="email" id="email" name="email" value="{form?.email ?? ''}"/>
-  <label for="message">Query</label>
-  {#if form?.messageMissing}<p class="error">The query field is required</p>{/if}
-  <textarea disabled id="message" name="message">{form?.message ?? ''}</textarea>
-  <button type="submit" disabled>Submit</button>
-</form>
-{#if form?.success}
- <p>Your message has been sent</p>
-{/if}
+<section>
+  <h2>Contact us</h2>
+  {#if loading}
+    <Spinner/>
+  {:else if success}
+    <p>Your message has been sent</p>
+  {:else}
+    <form method="POST" on:submit|preventDefault={handleSubmit}>
+      <label for="name">Name
+        {#if nameError}<span class="error">The name field is required</span>{/if}
+      </label>
+      <input id="name" name="name" bind:value={name}/>
+      {#if !loggedIn}
+        <label for="email">Email
+          {#if emailError}<span class="error">The email field is required</span>{/if}
+        </label>
+        <input type="email" id="email" name="email" bind:value={email}/>
+      {/if}
+      <label for="message">Query
+        {#if messageError}<span class="error">The query field is required</span>{/if}
+      </label>
+      <textarea id="message" name="message" bind:value={message}></textarea>
+      {#if !loggedIn}
+        <SvelteHcaptcha sitekey={import.meta.env.VITE_HCAPTCHA_SITE_KEY}
+                        theme="dark"
+                        reCaptchaCompa="false"
+                        on:success={captchaSuccess}
+                        on:error={captchaFail}
+                        on:expired={captchaFail}
+        />
+      {/if}
+      <button type="submit" disabled={!(loggedIn || captchaToken !== '')}>Submit</button>
+    </form>
+  {/if}
+</section>
