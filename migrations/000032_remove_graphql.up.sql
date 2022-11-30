352f201069ce625a32ffd79473eb2af27e468e83
Greg Holmes <git@greg.holmes.name>
2022-11-30 20:45:56 +0000

    Add a couple of migrations.
    1. Move http extension to internal to avoid end user's being able
       to call things.
    2. Remove some more graphql bits.

---
+++ migrations/000032_remove_graphql.up.sql
@@ -0,0 +1,4 @@
+DROP EVENT TRIGGER IF EXISTS graphql_watch_ddl;
+DROP EVENT TRIGGER IF EXISTS graphql_watch_drop;
+DROP EVENT TRIGGER IF EXISTS issue_graphql_placeholder;
+DROP EVENT TRIGGER IF EXISTS issue_pg_graphql_access;
