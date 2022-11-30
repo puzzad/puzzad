f744f25c0c7ef2746e7cac51cc4e0fc0fed78c53
Greg Holmes <git@greg.holmes.name>
2022-11-30 20:49:46 +0000

    Fix the last migration (might not work)

--- migrations/000032_remove_graphql.up.sql
+++ migrations/000032_remove_graphql.up.sql
@@ -1,4 +1,1 @@
-DROP EVENT TRIGGER IF EXISTS graphql_watch_ddl;
-DROP EVENT TRIGGER IF EXISTS graphql_watch_drop;
-DROP EVENT TRIGGER IF EXISTS issue_graphql_placeholder;
-DROP EVENT TRIGGER IF EXISTS issue_pg_graphql_access;
+DROP EXTENSION IF EXISTS pg_graphql CASCADE;
