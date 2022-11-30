473725e3080d24318527e6d55281a5eda0d51c97
Greg Holmes <git@greg.holmes.name>
2022-11-30 21:00:41 +0000

    Whilst im editing migrations I shouldnt, remove more graphql

--- migrations/000032_remove_graphql.up.sql
+++ migrations/000032_remove_graphql.up.sql
@@ -3,3 +3,4 @@ DROP EVENT TRIGGER IF EXISTS graphql_watch_ddl;
 DROP EVENT TRIGGER IF EXISTS graphql_watch_drop;
 DROP EVENT TRIGGER IF EXISTS issue_graphql_placeholder;
 DROP EVENT TRIGGER IF EXISTS issue_pg_graphql_access;
+DROP SCHEMA IF EXISTS graphql_public CASCADE;
