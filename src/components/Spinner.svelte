1fd8b212ffa1c77187dc75cf5e146cded1fe7d57
Chris Smith <chris@chameth.com>
2022-12-23 14:11:54 +0000

    Shiny new spinner

--- src/components/Spinner.svelte
+++ src/components/Spinner.svelte
@@ -6,27 +6,23 @@
 
   @keyframes spinner {
     0% {
-      transform: translate3d(-50%, -50%, 0) rotate(0deg);
+      transform: translate3d(0%, 0%, 0) rotate(0deg);
     }
     100% {
-      transform: translate3d(-50%, -50%, 0) rotate(360deg);
+      transform: translate3d(0%, 0%, 0) rotate(360deg);
     }
   }
 
-  .spin::before {
-    animation: 1.5s linear infinite spinner;
-    animation-play-state: inherit;
-    border: solid 5px var(--border);
-    border-bottom-color: var(--links);
-    border-radius: 50%;
-    content: '';
-    height: 40px;
-    width: 40px;
-    position: absolute;
-    transform: translate3d(-50%, -50%, 0);
+  div {
+    animation: 1s linear infinite spinner;
     will-change: transform;
+    animation-play-state: inherit;
+    font-size: 50px;
+    padding: 50px 0;
+    /* sepia is 39 degrees, our accent colour is 42 degrees, so rotate by 3 */
+    filter: grayscale(100%) sepia(100%) hue-rotate(3deg) contrast(40%) saturate(400%);
   }
 </style>
 <section>
-  <div class="spin"></div>
+  <div>🧩</div>
 </section>
