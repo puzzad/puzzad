8c260b9a141485ed316f07fd30edcafd6e82480d
Chris Smith <chris@chameth.com>
2023-06-26 20:52:00 +0100

    Move privacy policy into this repo
    This removes the need for database-based content. It makes the
    project harder to reuse for someone else, but simplifies it for
    our use...

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,11 +1,232 @@
 <script lang="ts">
   import {title} from '$lib/title'
-  import DatabasePage from '$components/DatabasePage.svelte'
 
   title.set('Puzzad: About & Privacy')
 </script>
 
 <h2>About &amp; Privacy</h2>
-<div class="about">
-  <DatabasePage page="privacy"></DatabasePage>
-</div>
+
+<section class="text">
+  <h3>About</h3>
+  <p>Puzzad is a trading name of Lunarian Limited, a company registered in England and Wales. Company number:
+    11937813.
+    Registered address: Lunarian Limited, International House, 24 Holborn Viaduct, City of London, United Kingdom,
+    EC1A
+    2BN.</p>
+</section>
+
+<section class="text">
+  <h3>Cookie Policy</h3>
+  <p>Cookies are text files containing small amounts of information, which your device downloads when you visit
+    websites.
+    When you return to the website, these are used to recognise your device.</p>
+  <p>We only use strictly necessary cookies required to provide authentication to adventures on the site. If you
+    remove them you would need to login again. We do not use cookies to track you, to provide advertisements and no
+    data from these would be passed to a third party.</p>
+</section>
+
+<section class="text">
+  <h3>Privacy policy</h3>
+  <p>This notice was last updated on the <strong>22nd of November 2022</strong>. A record of changes is available
+    at the end of this policy.</p>
+  <p>Puzzad is a trading name of Lunarian Limited which is registered with the UK Information Commissioner's Office.
+    Registration number: ZA521851.</p>
+  <p>If you have any concerns about how we handle your data, please contact us at privacy@puzzad.com, or using
+    <a href="/contact">our contact form.</a></p>
+
+  <h4>The type of personal information we collect</h4>
+  <p>When you use this website we may collect:</p>
+  <ul>
+    <li>Your device's internet protocol (IP) address</li>
+    <li>Any information entered into the “answer” box whilst solving puzzles</li>
+    <li>Details about your internet browser, internet service provider, and your interactions with the website</li>
+  </ul>
+  <p>When you register for an account on the site we may collect:</p>
+  <ul>
+    <li>Your email address</li>
+    <li>Basic profile details if you sign in via a third party provider</li>
+  </ul>
+  <p>When you contact us via email or contact form we may collect:</p>
+  <ul>
+    <li>Your device's internet protocol (IP) address.</li>
+    <li>Your name and email address.</li>
+    <li>Any other information that you choose to provide to us within the message itself.</li>
+  </ul>
+  <p>When you make a purchase on the website we may collect:</p>
+  <ul>
+    <li>Your billing details.</li>
+    <li>Information about the payment method used.</li>
+  </ul>
+
+  <h4>How and why we use your personal information</h4>
+  <h5>Your device's internet protocol (IP) address</h5>
+  <p>For website visitors, this is a technical necessity in order to deliver the website content to your device. We
+    may
+    use your IP address if required to combat fraud if required, but will not be used for other purposes.</p>
+  <p>For anyone that emails us, this may be supplied to us by your email provider as part of the message data. We do
+    not
+    explicitly process it, but it is technically infeasible to remove it, so we retain it in the same manner as your
+    name and email address, as described below.</p>
+  <h5>Your email address or basic third party profile</h5>
+  <p>If you register for an account this information will be stored, this is a technical necessity in order to allow
+    you
+    to securely sign in. We will also request consent to keep in touch for marketing purposes, this is optional and
+    not
+    required for site operation, you can revoke consent at any time.</p>
+  <h5>Your name and email address</h5>
+  <p>If you email us or fill out the contact form on our website, we will receive your name and email address. We use
+    these in order to reply to you, to action any enquiries you may have, and for any reporting or auditing purposes
+    depending on the nature of the message.</p>
+  <p>If in the course of sending us a message you supply us with any other personal information, this will be
+    processed
+    in the same manner as your name and email address, as described above.</p>
+  <h5>Your billing details and information about the payment method used</h5>
+  <p>If you make a purchase from us, we will receive information about the transaction including your name, billing
+    address and details about the payment method (such as the type of card used and the last four digits of the card
+    number). We store this information as part of our financial records, to use in case of dispute or fraud, and to
+    resolve any support requests that may occur in relation to the purchase.</p>
+  <h5>Information entered into the “answer” box</h5>
+  <p>During the course of playing our adventures you will need to enter guesses and answers, this is a free form
+    textbox
+    so any information can be entered here. We store any information entered here to analyse and improve puzzles,
+    diagnose issues, and generate statistics. This information is not associated with any personal information we
+    hold,
+    but we do not have a mechanism for filtering out personal information you may enter into the answer box.</p>
+  <h5>Details about your internet browser, internet service provider, and your interactions with the website</h5>
+  <p>
+    In order to prevent fraud and unwanted automated requests, we employ a 'captcha' system. As part of this
+    service, data about your browser, ISP, and details of how you use the website and interact with the 'captcha'
+    are recorded and used to determine if the action is being performed by a human or is automated.
+  </p>
+
+  <h4>Our lawful basis for processing your information</h4>
+  <p>Under the General Data Protection Regulation (GDPR), the lawful bases we rely on for processing this information
+    are:</p>
+  <ul>
+    <li>Consent - We may request to keep in touch with you, if we do so you revoke consent at any time.</li>
+    <li>We have a contractual obligation - In order to process your payment, we are required to store certain
+      information
+      about the transaction to ensure our contractual obligation to you.
+    </li>
+    <li>We have a legitimate interest - In order to run the website we require a functioning website and the ability
+      to
+      communicate via email. Transaction data must be stored to provide you with the product and to protect
+      against fraud.
+      We analyse your usage of the site to improve user experience and for improving puzzles.
+      We collect and analyse data to prevent fraud and automated abuse of our systems.
+    </li>
+    <li>Legal Obligations - As a VAT registered company we are required to keep records of transactions.</li>
+  </ul>
+
+  <h4>Who we share your personal information with</h4>
+  <p>Our email services are provided by Fastmail Pty Limited. If you email us, or your personal information is
+    otherwise
+    transmitted via email, this will necessarily be processed by Fastmail. This data transfer is subject to a
+    <a href="https://www.fastmail.com/about/dpa/">Data
+      Protection Addendum agreed with Fastmail</a>.</p>
+  <p>We use services provided by Mailgun Technologies Inc to process data submitted using the contact form on this
+    website, to send account related emails and to send marketing emails. This is limited to the information you
+    provide
+    in the contact form; we do not provide other information we may hold such as your device's internet protocol
+    (IP)
+    address. This data transfer is subject to a <a href="https://www.mailgun.com/legal/dpa/">Data Protection
+      Addendum agreed with Mailgun</a>.</p>
+  <p>Our payment services are provided by Stripe Inc. If you purchase a product from us, we are required to provide
+    certain information to Stripe so that they can process payment. This data transfer is subject to a <a
+        href="https://stripe.com/gb/legal/dpa">Data
+      Protection
+      Addendum agreed with Stripe</a>.</p>
+  <p>We use the services of hCaptcha, provided by Intuition Machines Inc, to protect our systems from automated abuse.
+    In order to effectively enable this protection, we share some basic information about your use of the website
+    with
+    hCaptcha. This includes your IP address, details of your web browser, details of your internet service provider,
+    and details of how you interact with the site. This data transfer is subject to a
+    <a href="https://newassets.hcaptcha.com/dpa/IMI.DPA.10.06.22.New.SCCs.UK.updates.pdf">Data Protection Addendum
+      agreed with Intuition Machines Inc</a>.</p>
+  <p>In the course of business, we may need to share your information with our lawyers, accountants, insurers, and
+    other
+    advisers.</p>
+  <p>If we suspect that criminal or potential criminal conduct has occurred, or if required by law, we may share data
+    with the appropriate lawful authority such as the police.</p>
+
+  <h4>How we store your personal information</h4>
+  <p>Email messages, and their associated metadata, are stored securely on servers operated by Fastmail Pty Limited.
+    We
+    require them to maintain email messages in a tamper-proof manner for a period of 7 years in order to comply with
+    our
+    legal obligations and as evidence in case of contract disputes or other litigation. After this time the message
+    will
+    be permanently deleted.</p>
+  <p>Transaction records (including billing details) are stored in a database securely in line with industry
+    standards.
+    We retain these records for a period of 7 years in order to comply with our legal record keeping obligations and
+    as
+    evidence in case of contract disputes or other litigation. After this time the records will be permanently
+    deleted.</p>
+  <p>Account information (including e-mail addresses and any basic profile details from a third-party) are stored in a
+    database securely in line with industry standards. We retain these details for one year after the last time the
+    account was used, but you can request we delete your account prior to this.</p>
+  <p>Anything entered into the answer box of a puzzle is stored in a database securely in line with industry
+    standards.
+    We retain this data for up to 1 year, in order to analyse and improve on our products. If you have entered
+    something
+    into the answer box that you wish to be removed, contact us for assistance.</p>
+  <p>If you consent to us sending you periodic e-mail messages then we will retain your email address for this purpose
+    until you withdraw consent. You can do this at any time using the “unsubscribe” link in the email. We store this
+    data in a database securely in line with industry standards.</p>
+  <p>All other information, such as the record of your computer’s IP address when visiting the site, is stored either
+    in
+    a database or in log files, in accordance with industry standards. We keep this data for 1 month in order to
+    detect
+    and prevent fraud and other abusive behaviour, respond to support queries, and monitor and improve our
+    services.</p>
+
+  <h4>Your data protection rights</h4>
+  <p>Under data protection law, you have rights including:</p>
+  <ul>
+    <li>Your right of access - You have the right to ask us for copies of your personal information.</li>
+    <li>Your right to rectification - You have the right to ask us to rectify personal information you think is
+      inaccurate.
+    </li>
+    <li>You also have the right to ask us to complete information you think is incomplete.</li>
+    <li>Your right to erasure - You have the right to ask us to erase your personal information in certain
+      circumstances.
+    </li>
+    <li>Your right to restriction of processing - You have the right to ask us to restrict the processing of your
+      personal
+      information in certain circumstances.
+    </li>
+    <li>Your right to object to processing - You have the right to object to the processing of your personal
+      information in
+      certain circumstances.
+    </li>
+    <li>Your right to data portability - You have the right to ask that we transfer the personal information you
+      gave us to
+      another organisation, or to you, in certain circumstances.
+    </li>
+  </ul>
+  <p>You are not required to pay any charge for exercising your rights. If you make a request, we have one month to
+    respond to you.</p>
+  <p>Please contact us as described above if you wish to make a request.</p>
+
+  <h4>How to complain</h4>
+  <p>If you have any concerns about our use of your personal information, you can make a complaint by contacting us as
+    above.</p>
+  <p>You can also complain to the ICO if you are unhappy with how we have used your data.</p>
+  <p>The ICO’s address:<br>
+    Information Commissioner’s Office<br>
+    Wycliffe House<br>
+    Water Lane<br>
+    Wilmslow<br>
+    Cheshire<br>
+    SK9 5AF</p>
+  <p>ICO Helpline number: 0303 123 1113</p>
+  <p>ICO website: https://www.ico.org.uk</p>
+
+  <h4>Changes to this policy</h4>
+  <h5>22nd November 2022</h5>
+  <p>Added data collection and transfer of data to hCaptcha required for preventing automated abuse.</p>
+  <p>Added lawful basis of legitimate interest in preventing fraud and automated abuse.</p>
+  <h5>15th November 2022</h5>
+  <p>Initial version.</p>
+</section>
