b3c3329e842263339a8415f6b2111531db9fce41
Greg Holmes <git@greg.holmes.name>
2023-07-09 17:08:22 +0100

    Show code for games again on list adventurebanner

--- src/routes/adventures/+page.svelte
+++ src/routes/adventures/+page.svelte
@@ -22,7 +22,7 @@
       <AdventureBanner
           status="{game.status}"
           adventureName='{game.expand.adventure?.name ?? "Unknown"}'
-          code="{game.code}"
+          code="{game.username}"
           isPublic="{game.expand.adventure?.public}"
       />
     {/each}
