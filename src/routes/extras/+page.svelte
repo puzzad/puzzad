7e9ebd4e5b04208d435a29990ba95e61232682c7
Greg Holmes <git@greg.holmes.name>
2023-07-12 15:43:49 +0100

    Pre-render some of the static pages

---
+++ src/routes/extras/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import {title} from '$lib/title'
+
+  title.set('Puzzad: Extras')
+</script>
+
+<h2>Extras</h2>
+
+<section class="text">
+  <h3>About</h3>
+  <p>We're working on additional resources, these will be available soon.</p>
+</section>
