02df0fd16d44ef9c3d4480c33e50d6cae47bb7a5
Chris Smith <chris@chameth.com>
2025-11-02 12:20:50 +0000

    Remove vite-validate-env
    The new version has changes that totally break how we're using it,
    so maybe we just.... don't?

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,9 @@
 import {sveltekit} from '@sveltejs/kit/vite'
 import type {UserConfig} from 'vite'
-import { ValidateEnv } from '@julr/vite-plugin-validate-env';
-import { z } from 'zod'
 
 const config: UserConfig = {
   plugins: [
     sveltekit(),
-    ValidateEnv({
-      validator: 'zod',
-      schema: {
-        VITE_API_URL: z.string().transform((value) => value.endsWith('/') ? value : `${value}/`),
-        VITE_HCAPTCHA_SITE_KEY: z.string().min(1)
-      }
-    })
   ],
   optimizeDeps: {
     force: true,
